0b44c399aade2632126bc0c7db426d684bbe8139
tudor stoica <tudor.stoica19@gmail.com>
2021-06-26 15:59:11 -0400

    tried fixing issues

---
+++ target/debug/deps/iota_identity_example.d
@@ -0,0 +1,5 @@
+/Users/daddy/Code/Hackathon App/ruhackathon/target/debug/deps/iota_identity_example: src/main.rs
+
+/Users/daddy/Code/Hackathon App/ruhackathon/target/debug/deps/iota_identity_example.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/ruhackathon.d
@@ -0,0 +1,5 @@
+/Users/daddy/Code/Hackathon App/ruhackathon/target/debug/deps/ruhackathon: src/main.rs
+
+/Users/daddy/Code/Hackathon App/ruhackathon/target/debug/deps/ruhackathon.d: src/main.rs
+
+src/main.rs:
