9d048d1469d83cc8969cce1ab9a8a239679ae6d7
Julia Groza <jgroza@uwo.ca>
2021-06-26 15:06:55 -0400

    added dependency

---
+++ vaccineApp/target/debug/deps/vaccineApp.d
@@ -0,0 +1,5 @@
+/Users/julia/ruhackathon/vaccineApp/target/debug/deps/vaccineApp: src/main.rs
+
+/Users/julia/ruhackathon/vaccineApp/target/debug/deps/vaccineApp.d: src/main.rs
+
+src/main.rs:
---
+++ vaccineApp/target/debug/vaccineApp.d
@@ -0,0 +1,1 @@
+/Users/julia/ruhackathon/vaccineApp/target/debug/vaccineApp: /Users/julia/ruhackathon/vaccineApp/src/main.rs
