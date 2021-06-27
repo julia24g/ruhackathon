9f0eab597651f6e754dc780fcd317ec9bdd42a0c
Julia Groza <jgroza@uwo.ca>
2021-06-26 20:14:28 -0400

    added link

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use std::io;
 
 use identity::account::Account;
 use identity::account::AccountStorage;
@@ -28,16 +29,25 @@ async fn main() -> Result<()> {
   // Retrieve the DID from the newly created Identity state.
   let document1: &IotaDID = snapshot1.identity().try_did()?;
 
-  println!("[Example] Local Snapshot = {:#?}", snapshot1);
-  println!("[Example] Local Document = {:#?}", snapshot1.identity().to_document()?);
-  println!("[Example] Local Document List = {:#?}", account.list_identities().await);
+  //println!("[Example] Local Snapshot = {:#?}", snapshot1);
+  //println!("[Example] Local Document = {:#?}", snapshot1.identity().to_document()?);
+  //println!("[Example] Local Document List = {:#?}", account.list_identities().await);
+
+  match io::stdin().read_line(&mut snapshot1.identity().this_message_id().to_string()){
+    Ok(_) => {
+      println!("Copy this link into a browser to view your secure vaccination certificate: https://explorer.iota.org/mainnet/message/{}", snapshot1.identity().this_message_id().to_string());
+    },
+    Err(e) => println!("Oops! Something went wrong: {}", e)
+  }
 
   // Fetch the DID Document from the Tangle
   //
   // This is an optional step to ensure DID Document consistency.
   let resolved: IotaDocument = account.resolve_identity(document1).await?;
 
-  println!("[Example] Tangle Document = {:#?}", resolved);
+  //println!("[Example] Tangle Document = {:#?}", resolved);
 
   Ok(())
+
+
 }
