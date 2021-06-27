c398989500cb76b3c4bf677d7a9a9f52ff5a61c8
tudor stoica <tudor.stoica19@gmail.com>
2021-06-26 22:57:22 -0400

    Made console do smth

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::io;
 
 use identity::account::Account;
-use identity::account::AccountStorage;
+use identity::account::Command;
 use identity::account::IdentityCreate;
 use identity::account::IdentitySnapshot;
 use identity::account::Result;
+use identity::core::json;
+use identity::core::FromJson;
+use identity::core::Url;
+use identity::credential::Credential;
+use identity::credential::Subject;
+use identity::crypto::KeyPair;
 use identity::iota::IotaDID;
-use identity::iota::IotaDocument;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -73,6 +78,24 @@ async fn main() -> Result<()> {
     Err(e) => println!("Oops! Something went wrong: {}", e)
   }
 
+  let mut antibody_test = String::new();
+  println!("Currently has COVID antibodies: ");
+  match io::stdin().read_line(&mut antibody_test){
+    Ok(_) => {
+      println!("You entered: {}", antibody_test.to_uppercase());
+    },
+    Err(e) => println!("Oops! Something went wrong: {}", e)
+  }
+
+  let mut active_covid_infection = String::new();
+  println!("SARS-CoV-2 positive (If true, specify date of positive test): ");
+  match io::stdin().read_line(&mut active_covid_infection){
+    Ok(_) => {
+      println!("You entered: {}", active_covid_infection.to_uppercase());
+    },
+    Err(e) => println!("Oops! Something went wrong: {}", e)
+  }
+
   // Create a new Account with Stronghold as the storage adapter
   let account: Account = Account::builder().build().await?;
 
@@ -80,11 +103,43 @@ async fn main() -> Result<()> {
   let snapshot1: IdentitySnapshot = account.create_identity(IdentityCreate::default()).await?;
 
   // Retrieve the DID from the newly created Identity state.
-  let document1: &IotaDID = snapshot1.identity().try_did()?;
+  let document: &IotaDID = snapshot1.identity().try_did()?;
 
-  //println!("[Example] Local Snapshot = {:#?}", snapshot1);
-  //println!("[Example] Local Document = {:#?}", snapshot1.identity().to_document()?);
-  //println!("[Example] Local Document List = {:#?}", account.list_identities().await);
+  // Add a new Ed25519 Verification Method to the identity
+  let command: Command = Command::create_method().fragment("key-1").finish()?;
+
+  // Process the command and update the identity state.
+  account.update_identity(document, command).await?;
+
+  // Create a subject DID for the recipient of a `UniversityDegree` credential.
+  let subject_key: KeyPair = KeyPair::new_ed25519()?;
+  let subject_did: IotaDID = IotaDID::new(subject_key.public().as_ref())?;
+
+  // Create the actual Verifiable Credential subject.
+  // I thought this part would add an atribute to the json file given in the link provided when running. It did not :(
+  let subject: Subject = Subject::from_json_value(json!({
+    "id": subject_did.as_str(),
+    "test_results": {
+      "vaccine_type_1": vaccine_type_1,     
+      "vaccine_date_1": vaccine_date_1,
+      "vaccine_provider_1": vaccine_provider_1,
+      "vaccine_type_2": vaccine_type_2,     
+      "vaccine_date_2": vaccine_date_2,
+      "vaccine_provider_2": vaccine_provider_2,
+      "antibody_test": antibody_test,
+      "active_covid_infection": active_covid_infection,
+    }
+  }))?;
+
+  // Issue an unsigned Credential...
+  let mut credential: Credential = Credential::builder(Default::default())
+    .issuer(Url::parse(document.as_str())?)
+    .type_("TestResults")
+    .subject(subject)
+    .build()?;
+
+  // ...and sign the Credential with the previously created Verification Method
+  account.sign(document, "key-1", &mut credential).await?;
 
   match io::stdin().read_line(&mut snapshot1.identity().this_message_id().to_string()){
     Ok(_) => {
@@ -93,14 +148,6 @@ async fn main() -> Result<()> {
     Err(e) => println!("Oops! Something went wrong: {}", e)
   }
 
-  // Fetch the DID Document from the Tangle
-  //
-  // This is an optional step to ensure DID Document consistency.
-  let resolved: IotaDocument = account.resolve_identity(document1).await?;
-
-  //println!("[Example] Tangle Document = {:#?}", resolved);
-
   Ok(())
-
 
 }
